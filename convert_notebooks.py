67d570de32b287efa07b91354b8d82cbd5395fd1
nulinspiratie <serwan.asaad@gmail.com>
2018-11-01 21:10:08 +1100

    feat: allow custom config

--- convert_notebooks.py
+++ convert_notebooks.py
@@ -1,3 +1,4 @@
+import sys
 import logging
 from src import (load_config,
                  NotebookFolder)
@@ -7,21 +8,18 @@ logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
 if __name__ == '__main__':
-    config = load_config('.\config.yml')
+    if len(sys.argv) == 1:
+        log.info('No config provided, using .\config.yml')
+        config_path = '.\config.yml'
+    else:
+        config_path = sys.argv[-1]
+        log.info(f'Using config {config_path}')
+    config = load_config(config_path)
 
     log.info('Parsing notebooks into LogFolder and LogNotebook objects')
     log_notebook_structure = NotebookFolder(path=config['base_dir'],
                                             name=config['name'],
                                             sections=config['sections'])
-
-    # log.info('Removing most notebooks')
-    # log_folder = log_notebook_structure.notebook_folders[1]
-    # log_notebook_structure.notebook_folders = [log_folder]
-    # log_folder.notebooks = []
-    # log_subfolder = log_folder.notebook_folders[0]
-    # log_folder.notebook_folders = [log_subfolder]
-    # log_subfolder.notebooks = log_subfolder.notebooks[:5]
-    # NotebookFolder.sections = {k: v for k, v in NotebookFolder.sections.items() if k == 'Analysis'}
 
     log.info('Creating index notebooks')
     log_notebook_structure.compile_index_notebook()
